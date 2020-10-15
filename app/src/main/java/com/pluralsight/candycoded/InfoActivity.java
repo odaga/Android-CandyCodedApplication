e90a91a4d8f7a98d0ad36adfe672d50e8d6ef099
OdagaJosh <34144362+odaga@users.noreply.github.com>
2020-10-15 23:44:31 +0300

    Added Dial intent to InfoActivity

--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -40,4 +40,8 @@ public class InfoActivity extends AppCompatActivity {
     // ***
     // TODO - Task 3 - Launch the Phone Activity
     // ***
+    public void createPhoneIntent(View view) {
+        Intent intent = new Intent(Intent.ACTION_DIAL,Uri.parse("tel:0123456789"));
+        startActivity(intent);
+    }
 }
